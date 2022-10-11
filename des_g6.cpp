37aed060b17854c31c08d590d8e90ef1725b1ab7
7usseiny-eg <omarelhussein423@gmail.com>
2022-10-11 17:04:07 +0200

    Some changes in the functions layout

--- des_g6.cpp
+++ des_g6.cpp
@@ -1,7 +1,50 @@
-#include <iostream>
+/********************************************** 
+* Module: Data Encryption Standard
+*
+* File Name: des_g6.cpp
+*
+* Description: This file includes all the needed algorithms for the DES Encryption/Decryption
+*
+* Author: Group 6
+*
+**********************************************/
 
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+
+/*              GLOBAL          VARIABLES                */
+char* plainTextPtr;
+char* keyPtr;
+char* encryptedTextPtr;
+char* plainText;
+char* key;
+char* encryptedText;
+#define MODULE_TEST 1
+//FOR TEST ONLY
+#if MODULE_TEST
+
+// plainText = "02468aceeca86420";
+// key = "0f1571c947d9e859";
+
+// plainTextPtr = plainText;
+// keyPtr = key;
+#endif
+
+void readPlainText(){
+
+}
+
+void DES_Encrypt(char* des_text, char* des_key){
+
+}
+
+void DES_Decrypt(char* des_encrypted_data, char* des_key){
+
+}
 
 int main(){
-    std::cout << "Insert your plain-text: ";
-    std::cin >> Plain_Text;
+
 }
