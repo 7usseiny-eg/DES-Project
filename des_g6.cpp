828e7469325976ad49dcc8866768bf2fdc834c04
mo3ta123 <omarmoataz123@outlook.com>
2022-10-13 19:27:15 +0200

    some functions are done

--- des_g6.cpp
+++ des_g6.cpp
@@ -25,9 +25,12 @@ uint16_t keyPtr;
 uint16_t encryptedText;
 uint16_t encryptedTextPtr;
 
+#define BLOCK_SIZE 64U
+
 /*      masks      */
 #define UPPER32(x) x >> 32
-#define LOWER32(x) x && (0x0000FFFF)
+#define LOWER32(x) x &(0x0000FFFF)
+#define checkBit(data, x) ((data >> x) & (1))
 
 #define MODULE_TEST 1
 // FOR TEST ONLY
@@ -81,32 +84,138 @@ expansion ON IT
 xor
 */
 /*mo3ta
-sbox
+sbox                (done)
 decryput
-round
-initialpermutation
+round               (done)
+initialpermutation  (done)
+inverse             (done)
 */
-uint64_t initialPermutation(uint64_t des_text)
+uint64_t initialPermutation(uint64_t block)
 {
-}
-uint64_t expansionETable(uint64_t des_text)
-{
-}
-uint64_t XOR(uint64_t input1, uint64_t input2, int bit_size)
-{
-    uint64_t result;
-    for (int i = 0; i < bit_size; i++)
+    const uint8_t table[BLOCK_SIZE] = {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7};
+    uint64_t result = 0;
+    for (int i = BLOCK_SIZE - 1; i >= 0; i--)
     {
+        result = (result << 1) | (checkBit(block, table[i] - 1));
     }
+    return result;
 }
-uint64_t sbox(uint64_t des_text)
+uint64_t expansionETable(uint64_t block)
 {
 }
-uint64_t permutation_p(uint64_t des_text)
+uint64_t XOR(uint64_t input1, uint64_t input2)
 {
 }
-uint64_t round(uint64_t des_text, uint64_t subkey)
+uint64_t sbox(uint64_t block_48)
 {
+    const uint8_t s_box[][4][16] = {
+        {
+            {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+            {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+            {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+            {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13},
+        },
+        {
+            {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+            {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+            {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+            {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9},
+        },
+        {
+            {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+            {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+            {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+            {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12},
+        },
+        {
+            {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+            {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+            {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+            {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14},
+        },
+        {
+            {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+            {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+            {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+            {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3},
+        },
+        {
+            {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+            {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+            {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+            {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13},
+        },
+        {
+            {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+            {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+            {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+            {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12},
+        },
+        {
+            {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+            {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
+            {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+            {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11},
+        },
+    };
+    uint64_t result32 = 0;
+    uint8_t shift = 0;
+    uint8_t working_on_6;
+    uint8_t row;
+    uint8_t col;
+    for (int box = 7; box >= 0; box--)
+    {
+        working_on_6 = block_48 & 0x3F;
+        row = ((working_on_6 >> 4) & 0x2) | ((working_on_6)&0x1);
+        col = (working_on_6 >> 1) & 0xF;
+        block_48 = block_48 >> 6;
+        result32 |= s_box[box][row][col] << shift;
+        shift += 4;
+        // cout << "working:" << (int)working_on_6 << "   row:" << (int)row << "  col:" << (int)col << "   result32:" << (int)result32 << "   shift:" << (int)shift << endl;
+    }
+    return result32;
+}
+uint64_t inverse_permutation(uint64_t block)
+{
+    const uint8_t table[] =
+        {
+            40, 8, 48, 16, 56, 24, 64, 32,
+            39, 7, 47, 15, 55, 23, 63, 31,
+            38, 6, 46, 14, 54, 22, 62, 30,
+            37, 5, 45, 13, 53, 21, 61, 29,
+            36, 4, 44, 12, 52, 20, 60, 28,
+            35, 3, 43, 11, 51, 19, 59, 27,
+            34, 2, 42, 10, 50, 18, 58, 26,
+            33, 1, 41, 9, 49, 17, 57, 25};
+    uint64_t result = 0;
+    for (int i = BLOCK_SIZE - 1; i >= 0; i--)
+    {
+        result = (result << 1) | (checkBit(block, table[i] - 1));
+    }
+    return result;
+}
+uint64_t permutation_p(uint64_t block)
+{
+}
+uint64_t round(uint64_t block, uint64_t subkey)
+{
+    uint64_t l = block >> 32;
+    uint64_t r = block & 0xFFFFFFFF;
+    uint64_t work;
+    work = expansionETable(r);
+    work = XOR(work, subkey);
+    work = sbox(work);
+    work = permutation_p(work);
+    l = XOR(work, l);
+    return (r << 32) | (l & 0xFFFFFFFF);
 }
 // uint64_t read64FromFile(string filePath, unsigned int shift){
 //     // IF THERE WAS A DELIMITER WE WILL BE USING GETLINE TO BUFFER THE STRING OF PLAINTEXT
@@ -195,5 +304,9 @@ void DES_Decrypt(vector<uint8_t> *des_encrypted_data, vector<uint8_t> des_key)
 }
 int main()
 {
-    DES_Encrypt(324234234234, "input.hex");
+    // DES_Encrypt(324234234234, "input.hex");
+    // cout << initialPermutation(0x123456789ABCDEF) << endl;
+    // cout << std::hex << sbox(0x6117BA866527) << endl;
+    // cout << std::hex << inverse_permutation(0xA4CD99543423234);
+    cout << std::hex << round(0xCC00CCFFF0AAF0AA, 0x1B02EFFC7072);
 }
